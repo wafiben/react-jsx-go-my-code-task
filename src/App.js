cbcf383d1571738999e77335c4fc0b6924b0ff4a
Wafi benjeddou <wafibenjeddou@gmail.com>
2021-06-19 18:42:38 +0200

    secondCommit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import './imageInSrc.jpg'
+import  imageInSrc from'./imageInSrc.jpg';
 
 function App() {
   return (
-    <div>
+    <div className="app">
   <div style={{border:'solid 1px black',maxWidth:'100vw'}}>
     <h1 className="title red">Your name here</h1>
     <br />
-    <img src={imageInSrc} />
+    <img src={imageInSrc}  id="first-img"/>
     <br />
-    <img src="/imageInPublic.jpg" />
+    <img src="/imageInPublic.jpg" id="second-img"/>
   </div>
   <video width={320} height={240} controls>
     <source src="myVideo.mp4" type="video/mp4" />
